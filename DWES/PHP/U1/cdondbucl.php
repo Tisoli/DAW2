44ece0de21b12b927468080988f048b4f98e8ce3
Tisoli <sihaoli365@gmail.com>
2026-09-17 12:28:50 +0200

    update 2026-09-17 12:28:50

---
+++ DWES/PHP/U1/cdondbucl.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Condicionales y bucles</title>
+</head>
+<body>
+    <h2>Condicionales</h2>
+    <?php
+    //Si la edad es manor a 18 que nuestres eres menor edad
+
+    $age = 17;
+    if($age >=18){
+        echo "<p>Eres mayor edad </p> ";
+
+        }else{
+
+            echo"<p>Eres menor edad </p> ";
+        }
+    $mensaje = $age >=18 ?"mayor":"menor";
+    echo $mensaje;
+
+    echo "<br>";
+
+    $dia = 2;
+    switch($dia){
+        case 1 :
+            echo"Lunes";
+            break;
+        case 2 :
+            echo"Martes";
+            break;
+        case 3 :
+            echo"Miercoles";
+            break;
+        default:
+            echo"Otro dia";
+            break;
+    }
+
+    echo "<br>";
+
+    $nombre = match($dia){
+        1 => "Lunes",
+        2 => "Martes",
+        3 => "Miercoles",
+        4 => "Jueves",
+        5 => "Viernes",
+        6,7 => "Fin de semana",
+        default => "No valido"
+        
+    };
+
+    echo $nombre;
+
+    ?>
+</body>
+</html>
