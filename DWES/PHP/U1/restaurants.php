6b3b1d043b8f4fc5933f8e85641c301485205e85
Tisoli <sihaoli365@gmail.com>
2026-09-23 10:38:07 +0200

    update 2026-09-23 10:38:07

---
+++ DWES/PHP/U1/restaurants.php
@@ -0,0 +1,12 @@
+<?php
+$pinoccion =[
+    "name" => "Carpaccion",
+    "address" => "Gran via 26",
+    "employees" => [4,7,2],
+    "quantity" =>[
+        "pizzas" => 3,
+        "drinks" => 8,
+        "desserts" =>4
+    ]
+]
+?>
