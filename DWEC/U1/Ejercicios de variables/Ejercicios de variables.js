1261600fb365ebfe9b0edb24aa1c20dd88c47ae4
Tisoli <sihaoli365@gmail.com>
2026-09-23 09:48:44 +0200

    update 2026-09-23 09:48:44

---
+++ DWEC/U1/Ejercicios de variables/Ejercicios de variables.js
@@ -0,0 +1,82 @@
+
+let edad = 20;
+let precio = 99.21;
+const PI = 3.14159;
+let nombre = "Sihao";
+let apellidos = "Li";
+let codigoPostal = "28021";
+let MayorEdad = true;
+const DIAS_SEMANA = 7;
+let resultado;
+let respuesta = null; 
+let temperatura = -25.2;
+let mensaje = "";
+let cantidad = "100";
+let activo = false;
+var contador = 0;
+
+// 1
+let i = "25";
+i = parseInt(i);
+if (i === 25) {
+    console.log("true");
+
+}
+
+// 2
+i = "12.8";
+i = parseFloat(i);
+if (i === 12.8) {
+    console.log("true");
+
+}
+
+// 3
+i = "12.8";
+i = parseInt(i);
+if (i === 12) {
+    console.log("true");
+
+}
+
+// 4
+i = "15abc";
+i = parseInt(i);
+if (i === 15) {
+    console.log("true");
+
+}
+
+// 5
+i = "18.5kg";
+i = parseFloat(i);
+if (i === 18.5) {
+    console.log("true");
+
+
+}
+
+// 6
+i = 100;
+i = 100 + "0"
+if (i === "100") {
+    console.log("true");
+
+}
+
+//7
+i = 1;
+i = Boolean(i)
+if (i === true) {
+    console.log("true");
+
+}
+
+// 8
+i = " ";
+i = Boolean(i)
+if (i === true) {
+    console.log("true");
+
+}
+
